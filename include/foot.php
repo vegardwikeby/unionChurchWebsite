54cf19bf0ca4ef68a724eaf5ee3bf5438592ff6e
gogvale <gogvale2011@gmail.com>
2021-01-20 15:21:00 -0600

    Change to Bootstrap Studio

--- include/foot.php
+++
@@ -1,7 +0,0 @@
-<!-- Loading the javaScript at the end of the page -->
-<script type="text/javascript" src="js/bootstrap.js"></script>
-<script type="text/javascript" src="js/jquery.prettyPhoto.js"></script>
-<script type="text/javascript" src="js/site.js"></script>
-</body>
-
-</html>
--- index.php
+++
@@ -1,9 +0,0 @@
-<?php include_once 'include/head.php'; ?>
-<?php include_once 'include/navbar.php'; ?>
-
-<?php include_once 'include/headwrap.php'; ?>
-<?php include_once 'include/services.php'; ?>
-<?php include_once 'include/contact.php'; ?>
-
-<?php include_once 'include/footer.php'; ?>
-<?php include_once 'include/foot.php'; ?>
